b468027fcd9fb504842c13e1d08ebc5d476431d9
Ilintseva.Valeria <ilintseva@mail.ru>
2024-11-12 13:50:09 +0300

    resulte

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class BmiService {
+    public int calculate(int weightKg, double heightM) {
+        double bmi = weightKg / (heightM * heightM); // расчет BMI
+        return (int) bmi; // приведение к целому числу
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98; // вес в килограммах
+        double height = 1.87; // рост в метрах
+        int bmi = service.calculate(weight, height); // должно получиться 28
+        System.out.println(bmi);
+    }
 }
